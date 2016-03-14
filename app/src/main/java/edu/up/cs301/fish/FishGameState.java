a71994f475203876452049a8108ed49f9ccfafe6
gmarston <marston18@up.edu>
2016-03-14 14:24:34 -0700

    Initial Commit

---
+++ app/src/main/java/edu/up/cs301/fish/FishGameState.java
@@ -0,0 +1,18 @@
+package edu.up.cs301.fish;
+
+import edu.up.cs301.game.infoMsg.GameState;
+
+/**
+ * Created by gisellemarston on 3/14/16.
+ */
+public class FishGameState extends GameState {
+
+
+
+
+
+
+
+
+
+}
